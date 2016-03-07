7f50d8e035b9f33743dbe7f5270b7d3a0e033552
Kasper Reijnders <kasper.reijnders@incentro.com>
2016-03-07 15:32:11 +0100

    removed redbackground

--- generators/app/templates/src/asset/javascript/site.js
+++ generators/app/templates/src/asset/javascript/site.js
@@ -9,15 +9,8 @@ import $ from 'jquery';
 import fastclick from 'fastclick';
 
 
-/**
- *
- */
-import redBackground from './behaviour/redBackground';
-
-
 $(function () {
     // Attach fastclick to body and all subelements
     fastclick(document.body);
 
-    redBackground();
 });
